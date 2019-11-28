fca24c2014e8285f69b2ceeb7848d6b56b1e37d5
Evan Welsh <2943271+rockon999@users.noreply.github.com>
2019-11-28 01:01:40 -0500

    Support computed properties and literals.

--- src/program/types/OptionalMemberExpression.js
+++ src/program/types/OptionalMemberExpression.js
@@ -1,21 +1,45 @@
 import Node from '../Node.js';
+import CompileError from '../../utils/CompileError.js';
 
 export default class OptionalMemberExpression extends Node {
 	transpile(code, transforms) {
 		super.transpile(code, transforms);
 
-		function buildChain(_obj) {
-			let obj = _obj;
-			let chain = [];
-			if (obj.name) {
-				chain.push(obj.name);
-			} else if (obj.object) {
-				chain.push(...buildChain(obj.object), obj.property.name);
+		function buildChain(expr) {
+			const { computed } = expr;
+			const chain = [];
+
+			if (expr.name) {
+				chain.push(expr.name);
+			} else if (expr.object && expr.property) {
+				const { object, property } = expr;
+
+				if (property.type === 'Literal') {
+					chain.push(...buildChain(object), `[${property.raw}]`);
+				} else if (property.type === 'Identifier') {
+					chain.push(...buildChain(object), computed ? `[${property.name}]` : property.name);
+				} else {
+					throw new CompileError(`Unknown property type found in OptionalMemberExpression: ${property.type}`);
+				}
+			} else {
+				throw new CompileError(`Unknown expression type passed to OptionalMemberExpression: ${expr.type}`);
 			}
+
 			return chain;
 		}
-		const chain = [...buildChain(this.object), this.property.name];
-		code.prependLeft(this.object.start, '(');
-		code.overwrite(this.object.end, this.property.end, ` == null ? void 0 : ${chain.join('.')})`);
+
+		function stringifyChain(chain) {
+			return chain.slice(1).reduce((prev, next) => {
+				if (next.startsWith('[')) {
+					return `${prev}${next}`;
+				} else {
+					return `${prev}.${next}`;
+				}
+			}, chain[0]);
+		}
+
+		const chain = [...buildChain(this)];
+		code.prependLeft(this.start, '(');
+		code.overwrite(this.object.end, this.end, ` == null ? void 0 : ${stringifyChain(chain)})`);
 	}
 }
